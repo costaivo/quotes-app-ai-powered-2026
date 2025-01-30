b16df3cc4b51e766432f9d400606b43a250fbe7f
Costa Ivo <ivo.costa@creativecapsule.com>
2025-01-30 23:57:02 +0530

    fixed code

--- fe/src/pages/AuthorPage.tsx
+++ fe/src/pages/AuthorPage.tsx
@@ -1,12 +1,28 @@
 export default function AuthorPage() {
-    return (
-      <div className="container mt-4">
-        <h1>Authors</h1>
-        <ul className="list-group mt-3">
-          <li className="list-group-item">Author 1</li>
-          <li className="list-group-item">Author 2</li>
-          <li className="list-group-item">Author 3</li>
-        </ul>
-      </div>
-    );
+  const authors = [
+    "A. P. J. Abdul Kalam",
+    "Gandhi",
+    "Ivo Costa",
+    "Jakob",
+    "John Lennon",
+    "Lao-Tze",
+    "Larry Niven",
+    "Linnie",
+    "Martin Fowler",
+    "Michael Feathers",
+    "Michael Jordan",
+    "Nelson Mandela",
+    "Steve Jobs",
+  ];
+
+  return (
+    <div className="container mt-4">
+      <h1>Authors</h1>
+      <ul className="list-group mt-3">
+        {authors.map((author, index) => (
+          <li className="list-group-item" key={index}>{author}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- fe/src/pages/QuotePage.tsx
+++ fe/src/pages/QuotePage.tsx
@@ -1,15 +1,40 @@
 export default function QuotePage() {
+    const quotes = [
+        {
+            text: "Be the change you wish to see in the world.",
+            author: "Mahatma Gandhi"
+        },
+        {
+            text: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+            author: "Albert Einstein"
+        },
+        {
+            text: "In three words I can sum up everything I've learned about life: it goes on.",
+            author: "Robert Frost"
+        },
+        {
+            text: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+            author: "Ralph Waldo Emerson"
+        },
+        {
+            text: "The only way to do great work is to love what you do.",
+            author: "Steve Jobs"
+        }
+    ];
+
     return (
-      <div className="container mt-4">
-        <h1>Random Quote</h1>
-        <div className="card mt-3">
-          <div className="card-body">
-            <blockquote className="blockquote mb-0">
-              <p>Quote content will appear here...</p>
-              <footer className="blockquote-footer mt-2">Author name</footer>
-            </blockquote>
-          </div>
+        <div className="container mt-4">
+            <h1>All Quotes</h1>
+            {quotes.map((quote, index) => (
+                <div className="card mt-3" key={index}>
+                    <div className="card-body">
+                        <blockquote className="blockquote mb-0">
+                            <p>{quote.text}</p>
+                            <footer className="blockquote-footer mt-2">{quote.author}</footer>
+                        </blockquote>
+                    </div>
+                </div>
+            ))}
         </div>
-      </div>
     );
-  }
+}
