107f918ea8f2d2cf3a3ec5152dc01880391fafd3
Ivo Costa <ivo.costa@creativecapsule.com>
2025-09-12 16:10:45 +0000

    chore: update commitlint configuration to allow longer commit headers and bodies

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // allow longer commit headers and bodies; keep conventional commits but relax line limits
+    'header-max-length': [2, 'always', 200],
+    'body-max-line-length': [2, 'always', 1000]
+  }
+};
