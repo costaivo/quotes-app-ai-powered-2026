39508ce601ef836d051da4a95ab903fe3622cdb5
icosta-cci <ivo.costa@creativecapsule.com>
2025-09-12 21:11:30 +0530

    feat: add SWC configuration and refine Jest settings for TypeScript support

--- app/be/jest.config.js
+++ app/be/jest.config.js
@@ -1,17 +1,9 @@
 module.exports = {
-  "moduleFileExtensions": [
-    "js",
-    "json",
-    "ts"
-  ],
-  "rootDir": "src",
-  "testRegex": ".*\\.spec\\.ts$",
-  "transform": {
-    "^.+\\.(t|j)s$": "@swc/jest"
+  transform: {
+    '^.+\\.(t|j)s$': '@swc/jest',
   },
-  // "collectCoverageFrom": [
-  //   "**/*.(t|j)s"
-  // ],
-  "coverageDirectory": "../coverage",
-  "testEnvironment": "node"
+  testEnvironment: 'node',
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
